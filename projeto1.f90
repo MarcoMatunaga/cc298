1476f3c93dac790d37da142a6442202022f9ec30
MarcoMatunaga <marco.matunaga@gmail.com>
2018-08-02 14:06:25 -0300

    boundary and initial conditions

--- projeto1.f90
+++ projeto1.f90
@@ -3,7 +3,9 @@ implicit none
 !
 !
 !
-real(8),dimension(:,:,:),allocatable             :: meshx, meshy, meshz
+real(8),dimension(:,:),allocatable               :: meshx, meshy	
+real(8),dimension(:,:),allocatable  		 :: T, p
+real(8)						 :: T_total, p_total
 integer(4)				         :: i,j,k
 integer(4)					 :: imax,jmax,kmax
 !
@@ -24,8 +26,8 @@ read(1,*) imax,jmax,kmax
 !
 !
 !
-allocate(meshx(imax,jmax,kmax), meshy(imax,jmax,kmax))
-allocate(meshz(imax,jmax,kmax))
+allocate(meshx(imax,jmax), meshy(imax,jmax))
+allocate(meshz(imax,jmax))
 !
 !
 call mesh
@@ -36,20 +38,62 @@ end program proj1
 !
 !
 !
+subroutine initial_conditions
+use vars
+implicit none
+!
+! inlet boundary
+! 
+! 1 lb/ft**2 = 47.880258888889
+! 1 R = 0.555556K
+!
+T_total = 0.555556d0*531.2d0
+P_total = 47.880258888889d0*2117.0d0
+!
+!
+!
+u = 0
+v = 0
+T = T_total
+p = p_total
+
+end subroutine initial_conditions
+!
+!
+!
+subroutine boundary_conditions
+use vars
+implicit none
+!
+! inlet boundary
+! 
+! 1 lb/ft**2 = 47.880258888889
+! 1 R = 0.555556K
+!
+T_total = 0.555556d0*531.2d0
+P_total = 47.880258888889d0*2117.0d0
+!
+!
+!
+u = 0
+v = 0
+T = T_total
+p = p_total
+end subroutine boundary_conditions
+!
+!
+!
 subroutine mesh
 use vars
 implicit none
 !
 ! construct the mesh 
 !
-do k = 1, kmax
 	do j = 1, jmax
 		do i = 1, imax
-			read(1,*) meshx(i,j,k),meshy(i,j,k),meshz(i,j,k)
+			read(1,*) meshx(i,j),meshy(i,j),meshz(i,j)
 		end do
 	end do
-end do
-print *, meshx(3,1,1)
 close(1)
 !
 ! arquivo tecplot
@@ -61,7 +105,7 @@ WRITE(2,*) 'ZONE I = ', imax, ' J =', jmax, ' DATAPACKING = POINT'
 do k = 1, kmax
 	do j = 1, jmax
 		do i = 1, imax
-			write(2,*) meshx(i,j,k),meshy(i,j,k)
+			write(2,*) meshx(i,j),meshy(i,j)
 		end do
 	end do
 end do
