158b30181d19ac88c77b53f9634a2e8658385237
marcomatunaga <marco.matunaga@gmail.com>
2018-10-01 02:31:54 -0300

    CFL_ramp

--- projeto1/projeto1.f90
+++ projeto1/projeto1.f90
@@ -5,6 +5,11 @@ program proj1
         use vars 
         use output_routines
         implicit none
+integer(4)              :: i_cfl
+integer(4)                                   :: ramp, CFL_ramp_size
+real(8),dimension(:,:),allocatable           :: CFL_ramp
+
+allocate(CFL_ramp(CFL_ramp_size,2))
 !
 !
 open(1,file='mesh') 
@@ -15,13 +20,13 @@ read(1,*) imax,jmax
     gama = 1.4d0
     delta_eta = 1.0d0
     delta_ksi = 1.0d0
-    CFL = 0.20d0
+    ! 
     !
     ! choose the time marching method method
     ! time_method = 1 euler explicit 
     ! time_method = 2 euler implicit (beam warming)
     !
-    time_method = 1
+    time_method = 2
     !
     ! choose dissipation (explicit)
     ! which_diss = 1 D4
@@ -34,7 +39,7 @@ read(1,*) imax,jmax
     !
     ! set dissipation parameters
     !
-    eps_dis_e = 10.0d0
+    eps_dis_e = 5.0d0
     eps_dis_i = 4.0d0*eps_dis_e
     !
     ! here we consider the value of cv as (5/2)*R
@@ -50,6 +55,32 @@ read(1,*) imax,jmax
     iter = 0
     max_iter = 25000
     a_cr = sqrt((2.0d0*gama)*((gama-1.0d0)/(gama+1.0d0))*c_v*T_total)
+    !
+    ! max CFL if the CFL_ramp is on
+    ! ramp = 1 there is a CFL ramp
+    ! ramp = 0 no
+    ramp = 1
+    !
+    !
+    CFL_ramp_size = 3
+    CFL = 20.00d0
+    ! ***********
+    ! outra possibilidade subir o CFL 
+    ! a cada determinado numero delta de iteracoes
+    ! ***********
+    !
+    ! CFL_ramp(i,j) = iter, CFL
+    !
+    CFL_ramp(1,1) = 0.0d0
+    CFL_ramp(2,1) = 100.0d0
+    ! deal this as percentage
+    CFL_ramp(3,1) = DBLE((3/100)*max_iter)
+    !
+    ! set the CFL numbers
+    !
+    CFL_ramp(1,2) = 0.150d0
+    CFL_ramp(2,2) = 15.0d0
+    CFL_ramp(3,2) = CFL
 !
 ! add one more point on the index j due to the symmetry line
 !
@@ -67,6 +98,7 @@ call initial_conditions_curv
 call boundary_conditions_curv
 call output_inicial
 max_residue = 1.0d0
+i_cfl = 1
 !
 !
 do while ( max_residue > -15.0d0 .and. iter < max_iter )
@@ -75,8 +107,12 @@ do while ( max_residue > -15.0d0 .and. iter < max_iter )
     do j = 1, jmax
             do i = 1, imax
                a(i,j) = sqrt(gama*p(i,j)*metric_jacobian(i,j)/Q_barra(i,j,1))
-               delta_t(i,j) = CFL/(max( abs(U_contravariant(i,j)) + a(i,j)*sqrt(ksi_x(i,j)**2.0d0 + ksi_y(i,j)**2.0d0 ), &
-                              abs(V_contravariant(i,j)) + a(i,j)*sqrt(eta_x(i,j)**2.0d0 + eta_y(i,j)**2.0d0 )))
+                if (ramp == 1 .and. iter == int(CFL_ramp(i_cfl,1)) ) then                
+                    CFL = CFL_ramp(i_cfl,2)
+                    i_cfl = i_cfl + 1
+                end if
+                delta_t(i,j) = CFL/(max( abs(U_contravariant(i,j)) + a(i,j)*sqrt(ksi_x(i,j)**2.0d0 + ksi_y(i,j)**2.0d0 ), &
+                                       abs(V_contravariant(i,j)) + a(i,j)*sqrt(eta_x(i,j)**2.0d0 + eta_y(i,j)**2.0d0 )))
             end do
     end do
     !
@@ -91,6 +127,7 @@ do while ( max_residue > -15.0d0 .and. iter < max_iter )
          call output_tecplot
     end if
     iter = iter + 1
+    write(*,*) iter, CFL
     !
     !
     call output_residue
@@ -104,6 +141,7 @@ close(5)
 !
 call output_final
 call deallocate_vars
+deallocate(CFL_ramp)
 !
 !
 close(1)
